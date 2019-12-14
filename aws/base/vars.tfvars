a0af8441da0645abea0ad6f0b1a754436b94db58
Tim Burns <timb@kagr.com>
2019-12-14 18:06:43 -0500

    vars for gateway

---
+++ aws/base/vars.tfvars
@@ -0,0 +1,5 @@
+region = "us-east-1"
+
+db_subnet_group_name = "default-vpc-0924a09c1e4270989"
+
+vpc_security_group_ids = ["sg-0eaea2d4088154d51", "sg-02664aba690d22d5e"]
--- aws/base/vpc-vars.tf
+++ aws/base/vpc-vars.tf
@@ -1,3 +1,15 @@
+variable "region" {
+	type = string
+}
+
+variable "db_subnet_group_name" {
+	type = string
+}
+
+variable "vpc_security_group_ids" {
+	type = "list"
+}
+
 provider "aws" {
   	version = "~> 2.7"
   	region = "us-east-1"
